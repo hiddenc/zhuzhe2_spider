b2b80c055d8c75c8f10fe706a4d5567c2c6b332b
ch <caobody@gmail.com>
2018-05-06 20:43:24 +0800

    complete

--- spider.js
+++ spider.js
@@ -1,72 +1,67 @@
-/**
- * Created by Administrator on 2018-05-05.
- */
-'use strict';
 const superagent = require('superagent');
-const reptileUrl = "http://api.zhuzhe2.com:9090/api/realTimeDgDealOpen";
-const async = require('async');
+const url = "http://api.zhuzhe2.com:9090/api/realTimeDgDealOpen";
+// const async = require('async');
 const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3308,
+    user: 'root',
+    password: 'root',
+    database: 'zhuzhe2'
+});
+connection.connect();
 
+//查
 
-//const connection = mysql.createConnection({
-//    host     : 'localhost',
-//    port     :  3308,
-//    user     : 'root',
-//    password : 'root',
-//    database : 'zhuzhe'
-//});
-//connection.connect();
-//var sql = 'SELECT * FROM data';
-////查
-//connection.query(sql,function (err, result) {
-//    if(err){
-//        console.log('[SELECT ERROR] - ',err.message);
-//        return;
-//    }
-//    console.log('--------------------------SELECT----------------------------');
-//    console.log(result);
-//    console.log('------------------------------------------------------------\n\n');
-//});
-//
-//connection.end();
-
-let max = 3; //2398;
+let max = 2410; //2411;
 let data = [];
+let gs = 1; //65
 
-async function returnData(){
-    await getData();
-    console.log(data);
-}
-
-function getData(){
-    for(let i = 1;i < max;i++){
-        superagent.get(reptileUrl)
-            .query({ time: '' })
-            .query({ pageSize: '40' })
-            .query({ curPage: i })
-            .set('Content-Type', 'application/json;charset=UTF-8')
+async function curl_get(url) {
+    console.log('begin');
+    for (let i = max; i > 0; i--) {
+        let sql = 'insert into data_all (houseName,useType,floorNum,buildID,projectID,buildArea,indoorArea,shareArea,beiAnPrice,beiAnAllPrice,dealPrice,dealAllPrice,projectName,buildName,dealDate) values ';
+        console.log("----第" + i + "页数据采集中");
+        await superagent
+            .get(url)
+            .query({time: ''})
+            .query({pageSize: '40'})
+            .query({curPage: i})
+            .set('Connection', 'keep-alive')
             .set('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36')
             .accept('application/json')
-            .end(function (err, res) {
-                if (res.ok) {
-                    //console.log(res.body.data);
-                    //console.log(Array.isArray(res.body.data.data));
-                    let list = res.body.data.data.filter(search);
-                    data = data.concat(list);
-                    console.log(data);
+            .then(function (res) {
+                if (res === undefined || res === null || res.statusCode === undefined || res.statusCode !== 200) {
+                    console.log("err");
                 } else {
-                    console.log('bad');
+                    return res.body.data.data;
                 }
-            });
-        console.log(i);
+            })
+            .then(function (res) {
+                for (let l = 0; l < 40; l++) {
+                    let tmp = "('" + res[l].houseName + "','" + res[l].useType + "','" + res[l].floorNum + "'," + res[l].buildID + ",'" + res[l].projectID + "'," + res[l].buildArea + "," + res[l].indoorArea + "," + res[l].shareArea + "," + res[l].beiAnPrice + "," + res[l].beiAnAllPrice + "," + res[l].dealPrice + "," + res[l].dealAllPrice + ",'" + res[l].projectName + "','" + res[l].buildName + "','" + res[l].dealDate + "')" + (l === 39 ? ';' : ',');
+                    sql += tmp;
+                }
+                console.log("第" + i + "页数据组合完毕，上传中");
+                connection.query(sql,function (err, result) {
+                    if(err){
+                        // console.log('[SELECT ERROR] - ',err.message);
+                        // return;
+                        throw err;
+                    }
+                    console.log('--------------------------INSERT----------------------------');
+                    console.log(result);
+                    console.log('------------------------------------------------------------\n\n');
+                });
+            })
     }
+
+    connection.end();
+    console.log('结束');
 }
 
-function search(value){
-    return value.floorNum === '14';
+function search(value) {
+    return value.buildID === 13834;
 }
 
-returnData();
-
-
-
+curl_get(url);
